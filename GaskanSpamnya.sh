7749b1c8f6870185984ebf76a8efeaf4897b43ee
Cy-Explor <51297931+Cy-Explor@users.noreply.github.com>
2019-06-03 04:14:35 +0700

    Create GaskanSpamnya.sh

---
+++ GaskanSpamnya.sh
@@ -0,0 +1,159 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "Cy-Spam"
+sleep 1
+echo "\033[33;1m Pilih Salah satu boejank"
+sleep 1
+echo "\033[36;1m★""\033[34;1mCyber Nine_Anon""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mSpam Bukalapak"
+echo "\033[35;1m2.""\033[36;1mSpam Telkomsel"
+echo "\033[35;1m3.""\033[36;1mSpam Grab"
+echo "\033[35;1m4.""\033[36;1mSpam Tokopedia"
+echo "\033[35;1m5.""\033[36;1mSpam CodaShop"
+echo "\033[35;1m6.""\033[36;1mSpam Surveyon"
+echo "\033[35;1m7.""\033[36;1mSpam Custom"
+echo "\033[31;1m0. Keluar boejank"
+echo "\033[37;1m81.""\033[30;1mInformasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Spam Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatasnya:""\033[37;1mUnlimited boejank"
+echo "\033[31;1mPemogramannya:""\033[37;1mPHP boejank"
+sleep 5
+clear
+cd $HOME/Cy-Spam/Tul/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Spam T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatasnya:""\033[37;1mUnlimited boejank"
+echo "\033[31;1mPemogramannya:""\033[37;1mPHP boejank"
+sleep 5
+clear
+cd $HOME/Cy-Spam/Tul/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Spam Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatasnya:""\033[37;1mUnlimited boejank"
+echo "\033[31;1mPemogramannya:""\033[37;1mPHP boejank"
+echo "\033[31;1mAuthor:""\033[37;1mRaja Adtiya Candra"
+sleep 5
+clear
+cd $HOME/Cy-Spam/Tul/
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Spam Tokopedia"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatasnya:""\033[37;1m1 Kali aja boejank"
+echo "\033[31;1mPemogramannya:""\033[37;1mPHP boejank"
+sleep 5
+clear
+cd $HOME/Cy-Spam/Tul/
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant -F gay "Spam CodaShop"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatasnya:""\033[37;1mUnlimited boejank"
+echo "\033[31;1mPemogramannya:""\033[37;1mPHP boejank"
+echo
+echo
+echo "\033[33;1mCOPY DULU LISENSI INI BOEJANK:" "\033[36;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
+sleep 1
+echo "\033[32;1mAne tunggu 5 Detik Boejank"
+sleep 5
+clear
+cd $HOME/Cy-Spam/Tul/
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Surveyon "
+echo "\033[31;1mJenis Spam:""\033[37;1mEmail"
+echo "\033[31;1mBatasnya:""\033[37;1mUnlimited boejank"
+echo "\033[31;1mPemogramannya:""\033[37;1mPHP boejank"
+sleep 5
+cd $HOME/Cy-Spam/Tul/
+php 6.php
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "Spam Custom"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m5 boejank"
+echo "\033[31;1mPemograman:""\033[37;1mntahlah, ane lupa boejank"
+echo
+echo
+sleep 6
+smsid boom -y
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "Cy-Spam"
+echo "\033[34;1mContact:""\033[37;1m https://fb.me/IrsyadMaulana81"
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: Mr.Cy-Arr"
+sleep 1
+echo "\033[36;1mTeam: Cyber Nine_anon"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m MR.WH5"
+sleep 8
+sh LITESPAM.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar Ngak Boejank"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi ya boejank :)"
+sleep 1
+fi
